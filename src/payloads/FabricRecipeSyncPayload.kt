649ce5239eefd209435786c5fbc84ce96afac1b0
illyrius666 <28700752+illyrius666@users.noreply.github.com>
2026-05-04 14:26:15 +0200

    using binary isntead of json since thats what fabric expects
    Signed-off-by: illyrius666 <28700752+illyrius666@users.noreply.github.com>

--- src/payloads/FabricRecipeSyncPayload.kt
+++ src/payloads/FabricRecipeSyncPayload.kt
@@ -47,9 +47,9 @@ internal data class FabricRecipeSyncPayload(
                 buf.writeResourceKey(recipe.id())
 
                 @Suppress("UNCHECKED_CAST")
-                val codec = this.serializer.codec().codec() as Codec<Any>
-
-                buf.writeJsonWithCodec(codec, recipe.value())
+                val streamCodec =
+                    ByteBufCodecs.fromCodecWithRegistries(this.serializer.codec().codec() as Codec<Recipe<*>>)
+                streamCodec.encode(buf, recipe.value())
             }
         }
 
@@ -81,9 +81,13 @@ internal data class FabricRecipeSyncPayload(
 
                 repeat(count) {
                     val id = buf.readResourceKey(Registries.RECIPE)
-                    val recipe = buf.readLenientJsonWithCodec(recipeSerializer.codec().codec())
 
-                    recipes.add(RecipeHolder(id, recipe as Recipe<*>))
+                    @Suppress("UNCHECKED_CAST")
+                    val streamCodec =
+                        ByteBufCodecs.fromCodecWithRegistries(recipeSerializer.codec().codec() as Codec<Recipe<*>>)
+                    val recipe = streamCodec.decode(buf)
+
+                    recipes.add(RecipeHolder(id, recipe))
                 }
 
                 return Entry(recipeSerializer, recipes)
